ca15a05500bbfe07824c91127cedec37ef25549f
alex klee <alex.klee@outlook.com>
2020-02-15 15:44:59 +0100

    add word-rank

---
+++ word-rank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace word_rank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
