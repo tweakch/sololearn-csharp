c9476d0e365cc2e7e7d2b63fcd868e48224c6b53
alex klee <alex.klee@outlook.com>
2020-02-15 17:26:39 +0100

    optimized solution

--- word-rank/Program.cs
+++ word-rank/Program.cs
@@ -9,27 +9,46 @@ namespace SoloLearn
 {
     class Program
     {
+        static List<string> _list = new List<string>(); 
         static void Main(string[] args)
         {
-            var word = Console.ReadLine();
-            var list = Permute(word).Distinct().ToList();
-            list.Sort(); 
-            Console.WriteLine(list.IndexOf(word));
-        }
-        static List<string> Permute(string word)
-        {
-            var p = new List<string>();
+            string str = Console.ReadLine();
+            char[] arr = str.ToCharArray();
+            Permute(arr);
+            _list.Sort();
 
-            if(word.Length == 1)  {
-                p.Add(word);
-            } else {
-                for(int i = 0; i < word.Length; i++) {
-                    foreach(var sw in Permute(word.Remove(i,1))){
-                        p.Add(word[i] + sw);        
-                    }
-                }
+            Console.WriteLine(_list.Distinct().ToList().IndexOf(str)+1);
+        }
+
+        private static void Swap(ref char a, ref char b)
+        {
+            if (a == b) return;
+
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Permute(char[] list)
+        {
+            int x = list.Length - 1;
+            GetPer(list, 0, x);
+        }
+
+        private static void GetPer(char[] list, int k, int m)
+        {
+            if (k == m)
+            {
+                _list.Add(new string(list));
             }
-            return p;
+            else
+                for (int i = k; i <= m; i++)
+                {
+                    Swap(ref list[k], ref list[i]);
+                    GetPer(list, k + 1, m);
+                    Swap(ref list[k], ref list[i]);
+                }
         }
     }
+
 }
