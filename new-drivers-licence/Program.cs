f90080db857dbb313f910261b2d99dfc05ce1526
Alexander Klee <aklee@pdc-online.com>
2020-02-17 15:31:50 +0100

    solve with chunk

--- new-drivers-licence/Program.cs
+++ new-drivers-licence/Program.cs
@@ -11,15 +11,34 @@ namespace new_drivers_licence
             var name = Console.ReadLine();
             var agents = Convert.ToInt32(Console.ReadLine());
             var queue = Console.ReadLine().Split(" ").ToList();
+            
             var time = 0;
 
             queue.Add(name);
-            foreach(var items in queue.OrderBy(n => n).Take(agents)){
+            queue.Sort();
+
+            foreach (var items in queue.Chunk(agents))
+            {
                 time += 20;
                 if (items.Contains(name)) break;
             }
-            
+
             Console.WriteLine(time);
+        }
+    }
+
+    static class ListExtensions
+    {
+        ///<summary>
+        /// Break a list of items into chunks of a specific size
+        /// </summary>
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunksize)
+        {
+            while (source.Any())
+            {
+                yield return source.Take(chunksize);
+                source = source.Skip(chunksize);
+            }
         }
     }
 }
